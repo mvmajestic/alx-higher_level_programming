6fd561cf6e95af4b3cb0d5fdf04c18b07190339a
root <vuyisanani@ymail.com>
2023-12-08 14:12:49 -0800

    Task 17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -51,3 +51,16 @@ class Base:
                 jsonfile.write(Base.to_json_string(list_dicts))
 
 
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the deserialization of a JSON string.
+        Args:
+            json_string (str): A JSON str representation of a list of dicts.
+        Returns:
+            If json_string is None or empty - an empty list.
+            Otherwise - the Python list represented by json_string.
+        """
+        if json_string is None or json_string == "[]":
+            return []
+        return json.loads(json_string)
+
