4ae2ab0b8525c121fff72f7067e8bc037147317b
root <vuyisanani@ymail.com>
2023-12-08 13:02:29 -0800

    Task 9

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -130,6 +130,16 @@ class Rectangle(Base):
         except IndexError:
             pass
 
+    def to_dictionary(self):
+        '''
+            Returns a dictionary representation of this class
+        '''
+        return {'x': getattr(self, "x"),
+                'y': getattr(self, "y"),
+                'id': getattr(self, "id"),
+                'height': getattr(self, "height"),
+                'width': getattr(self, "width")}
+
 
 
 
